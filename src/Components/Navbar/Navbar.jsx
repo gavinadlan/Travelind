d4f5363a410468cf333e756e2300fb6635562979
gavin adlan hidayat <gavinadlan@gmail.com>
2025-02-03 23:22:22 +0700

    Initial Commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./navbar.css";
+import { MdOutlineTravelExplore } from "react-icons/md";
+
+const Navbar = () => {
+  return;
+  <section className="navBarSection">
+    <header className="header flex">
+      <div className="logoDiv">
+        <a href="#" className="logo flex">
+          <h1>
+            <MdOutlineTravelExplore className="icon" />
+            TravelInd
+          </h1>
+        </a>
+      </div>
+    </header>
+  </section>;
+};
+
+export default Navbar;
---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./app.css";
+import Home from "./Components/Home/Home.jsx"; // Perbaiki path dan ekstensi
+import Navbar from "./Components/Navbar/Navbar.jsx"; // Perbaiki path dan ekstensi
+import Main from "./Components/Main/Main.jsx"; // Perbaiki path dan ekstensi
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Main />
+    </>
+  );
+};
+
+export default App;

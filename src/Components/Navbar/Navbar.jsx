5cfd1ed65baddf7d7eeef685e8c3088db76f0ccd
gavin adlan hidayat <gavinadlan@gmail.com>
2025-02-04 00:18:38 +0700

    Initial Commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./navbar.css";
 import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
   return (
@@ -13,6 +15,50 @@ const Navbar = () => {
               TravelInd
             </h1>
           </a>
+        </div>
+
+        <div className="navBar">
+          <ul className="navlists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">Home</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">Packages</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">Shop</a>
+            </li>
+            
+            <li className="navItem">
+              <a href="#" className="navLink">About</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">Pages</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">News</a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">Contact</a>
+            </li>
+
+            <button className="btn">
+              <a href="#">BOOK NOW</a>
+            </button>
+          </ul>
+
+          <div className="closeNavBar">
+            <AiFillCloseCircle className="icon"/>
+          </div> 
+        </div>
+
+        <div className="toogleNavbar">
+          <TbGridDots className="icon"/>
         </div>
       </header>
     </section>
